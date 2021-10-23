feb791b554d0fb5701ff68ca9eaf32aece5d0f78
Ahmed Ghonaime <67119971+ghonaime91@users.noreply.github.com>
2021-10-23 14:09:38 +0200

    Add files via upload

---
+++ DB/cancel.php
@@ -0,0 +1,28 @@
+<?php
+require "../helpers/db_connection.php";
+require "../helpers/validator.php";
+require "../helpers/checklogin.php";
+#course id validation
+$id = $_GET['id'];
+$player_id = $_SESSION['user']['id'];
+if(!validate($id,"is_int")) {
+    echo "Wrong id you will redirect to our courses page!";
+    header( "refresh:2;url= courses.php" );
+    exit;
+}
+$sql = "delete from courses where player_id = $player_id and matrial_id = $id"  ;
+$op  = mysqli_query($con,$sql);
+
+
+if($op) {
+    echo "<h1 style='color:blue; margin:auto;'>You have been canceld the course</h1><br>
+    <h3>You will be redirected to your courses page</h3>";
+    header( "refresh:2;url= player.php" );
+    exit;
+}
+else {
+    echo "Error try again";
+}
+
+
+?>
